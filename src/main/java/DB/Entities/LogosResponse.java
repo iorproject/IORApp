9045d0c25b56b8c38a1382d9d9f0369786df7511
OmerBlechman <37245909+OmerBlechman@users.noreply.github.com>
2019-08-05 20:18:09 +0300

    allow to get logos

---
+++ src/main/java/DB/Entities/LogosResponse.java
@@ -0,0 +1,11 @@
+package main.java.DB.Entities;
+
+import java.util.Map;
+
+public class LogosResponse {
+    private Map<String,String> logos;
+
+    public Map<String, String> getLogos() {
+        return logos;
+    }
+}

a0210a2c06e3985f78aeda4d02f1f447453525cd
Or Barak <barakor21@gmail.com>
2019-08-27 20:27:36 +0300

    or algo

--- server/src/servlets/InitProcessor.java
+++ server/src/servlets/InitProcessor.java
@@ -4,8 +4,10 @@ import EmailProcessor.Processor;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+@WebServlet(name = "InitProcessor", urlPatterns = {"/InitProcessor"}, loadOnStartup = 1)
 public class InitProcessor extends HttpServlet {
 
     public void init(ServletConfig config) throws ServletException{
